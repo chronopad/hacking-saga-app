47c2f17cb8bfc0597b4ec450cc951b358489f00b
chrono <chrono@gmail.com>
2025-05-01 23:11:39 +0700

    Database setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
 const app = express();
 
-// Main API route, add route at ./routes/auth.route.js
+const PORT = process.env.PORT;
+
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-    console.log("Server is running on port 5001")
+app.listen(PORT, () => {
+    console.log("Server is running on port: " + PORT);
+    connectDB();
 });
