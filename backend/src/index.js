e284d9c2bd2824b826a2481275f9e02a5c312e2e
chrono <chrono@gmail.com>
2025-05-02 13:45:31 +0700

    Added message routes for message API

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookieparser from "cookie-parser";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port: " + PORT);
