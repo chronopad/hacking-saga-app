153a9641095bd767adb2d2ae23268d9fc32a6933
chrono <chrono@gmail.com>
2025-05-07 14:20:24 +0700

    Added access to the chat page via Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
-import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import { LogOut, MessageSquare, Settings, User, MessageCircleMore } from "lucide-react";
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
@@ -38,6 +38,11 @@ const Navbar = () => {
                 <Link to={"/profile"} className={`btn btn-sm gap-2`}>
                   <User className="size-5" />
                   <span className="hidden sm:inline">Profile</span>
+                </Link>
+
+                <Link to={"/chat"} className={`btn btn-sm gap-2`}>
+                  <MessageCircleMore className="size-5" />
+                  <span className="hidden sm:inline">Message</span>
                 </Link>
 
                 <button className="flex gap-2 items-center" onClick={logout}>
