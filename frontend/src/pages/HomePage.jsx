5296e791407adb016f2f483a2abcc8ec1fad3439
chrono <chrono@gmail.com>
2025-05-07 14:06:48 +0700

    Added a temporary home page, chat moved to chat page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,24 +1,14 @@
-import { useChatStore } from "../store/useChatStore";
-
-import Sidebar from "../components/Sidebar";
-import NoChatSelected from "../components/NoChatSelected";
-import ChatContainer from "../components/ChatContainer";
-
 const HomePage = () => {
-  const { selectedUser } = useChatStore();
-
-  return (
-    <div className="h-screen bg-base-200">
-      <div className="flex items-center justify-center pt-20 px-4">
-        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
-          <div className="flex h-full rounded-lg overflow-hidden">
-            <Sidebar />
-
-            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
-          </div>
+    return (
+        <div className="min-h-screen bg-white flex items-center justify-center p-4">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold text-gray-800 mb-4">🚧 bntr yh</h1>
+                <p className="text-gray-600 text-lg">
+                    ini hom peig lg dibwat
+                </p>
+                <button class="btn btn-primary mt-6">tombol matching</button>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+      );
 };
 export default HomePage;
